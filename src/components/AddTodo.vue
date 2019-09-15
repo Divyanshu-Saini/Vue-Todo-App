e4b8fd92e1376c5867d95319db9736eb41aef769
Divyanshu-Saini <bat2268@gmail.com>
2019-09-15 13:15:26 +0530

    completed vue todo app

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,45 @@
+<template>
+    <div>
+        <form @submit.prevent="addTodo">
+            <input type="text" name="title" placeholder="Add todo..." v-model="title"/>
+            <input type="submit" value="Submit" class="btn"/>
+        </form> 
+    </div>
+</template>
+
+<script>
+// import uuid from 'uuid';
+export default {
+  name: "AddTodo",
+  data(){
+      return{
+          title:''
+      }
+  },
+  methods:{
+      addTodo(){
+        //   e.preventDefault()
+          const newTodo = {
+            //   id:uuid.v4(),
+              title:this.title,
+              completed:false
+          };
+          this.$emit('add-todo', newTodo);
+          this.title = '';
+      }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+input[type="text"] {
+  flex: 18;
+  padding: 5px;
+}
+input[type='submit'] {
+  flex: 2;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <h1>This is Todolis app deeeveloped using vue js !</h1>
+  </div>
+</template>
